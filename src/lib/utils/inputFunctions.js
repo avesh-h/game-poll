61d303bb52ee15ab3a2260d8790f3decd58e6a7f
Avesh <aveshhasanfatta1155@gmail.com>
2024-05-23 23:12:07 +0530

    deployed error2

--- src/lib/utils/inputFunctions.js
+++ src/lib/utils/inputFunctions.js
@@ -1,6 +1,6 @@
 export const fnPressNumberKey = (e) => {
   if (
-    (e.charCode !== 46 || e.target.value.includes(".")) &&
+    (e.charCode !== 46 || e.target.value.includes('.')) &&
     !(e.charCode >= 48 && e.charCode <= 57)
   ) {
     e.stopPropagation();
