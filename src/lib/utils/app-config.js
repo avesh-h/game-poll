1b8ff1cbc8b4cf3457190277c7e0363a613050f0
Avesh <aveshhasanfatta1155@gmail.com>
2024-06-27 22:00:39 +0530

    feat : render deployment

--- src/lib/utils/app-config.js
+++ src/lib/utils/app-config.js
@@ -3,7 +3,8 @@ const AppConfig = {
     apiUrl: 'http://localhost:3000',
   },
   production: {
-    apiUrl: 'https://game-poll.vercel.app',
+    // apiUrl: 'https://game-poll.vercel.app',
+    apiUrl: 'https://play-o-time.onrender.com',
   },
 };
 
