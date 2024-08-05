c1e06b33cee28b749245bd08c1fa4372d1a9015d
Avesh <aveshhasanfatta1155@gmail.com>
2024-08-05 23:48:03 +0530

    fix : quick fix

--- src/app/api/messageQueue/route.js
+++ src/app/api/messageQueue/route.js
@@ -5,6 +5,7 @@ import client from '@/lib/redisConfig';
 //This is redis testing route for queue
 export const GET = async () => {
   console.log('API called!');
+  return NextResponse.json({ message: 'Message queue' });
 
   //REDIS MESSAGE QUEUES
   // try {
@@ -19,15 +20,15 @@ export const GET = async () => {
   // }
 
   // REDIS PUB SUB
-  try {
-    const res = await client.publish(
-      'messageQueue',
-      JSON.stringify({ id: 123, name: 'famous', age: 22 })
-    );
-    console.log('res', res);
-    return NextResponse.json({ message: 'success' });
-  } catch (error) {
-    console.error('Error pushing to Redis:', error);
-    return NextResponse.json({ message: 'error', error: error.message });
-  }
+  // try {
+  //   const res = await client.publish(
+  //     'messageQueue',
+  //     JSON.stringify({ id: 123, name: 'famous', age: 22 })
+  //   );
+  //   console.log('res', res);
+  //   return NextResponse.json({ message: 'success' });
+  // } catch (error) {
+  //   console.error('Error pushing to Redis:', error);
+  //   return NextResponse.json({ message: 'error', error: error.message });
+  // }
 };
